e63e2bb1a41cae5a2f98efb8bd15207ba165e9b3
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 16:32:09 +0530

    wages for month assuming 20 days

--- EmpWage.js
+++ EmpWage.js
@@ -1,23 +1,33 @@
-///UC2 --> Calculate Daily Employee Wage using switch
-
+///UC4 -->wages for month assuming 20 days
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
+const WAGE_PER_HR = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS =20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+
+    switch(empCheck){
+        case IS_PART_TIME:
+        case IS_FULL_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
+let empHrs = 0;
+let empCheck =Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Emp Wage: " + empWage);
+for (let day = 0; day< NUM_OF_WORKING_DAYS; day++){
+    let empCheck =Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);    
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC2 - Emp Wage: " + empWage);
+console.log("Total Hours: "+ empHrs +"   Emp Wage: "+empWage);
