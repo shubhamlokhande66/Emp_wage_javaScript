c811313018b14e40b808191e55de165769c42578
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 16:39:04 +0530

    write a function for daily working hours

--- EmpWage.js
+++ EmpWage.js
@@ -1,17 +1,12 @@
-///UC4 -->wages for month assuming 20 days
+//UC3 -->  write a function for daily working hours
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const WAGE_PER_HR = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS =20;
-
 function getWorkingHours(empCheck) {
-
     switch(empCheck){
         case IS_PART_TIME:
-        case IS_FULL_TIME:
             return PART_TIME_HOURS;
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
@@ -19,15 +14,8 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-
 let empHrs = 0;
 let empCheck =Math.floor(Math.random() * 10) % 3;
 empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HR;
 console.log("Emp Wage: " + empWage);
-for (let day = 0; day< NUM_OF_WORKING_DAYS; day++){
-    let empCheck =Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);    
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours: "+ empHrs +"   Emp Wage: "+empWage);
