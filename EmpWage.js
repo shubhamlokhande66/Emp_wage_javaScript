a813c6ae4608c931dcc075282ff6f0bac4d9e030
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 18:17:59 +0530

    Object Opertion Using Arrow Function

--- EmpWage.js
+++ EmpWage.js
@@ -1,21 +1,19 @@
-//UC10 object creation
-{
-    let totalEmpHrs = 0;
-    let totalWorkingDays = 0;
-    let empDailyHrsAndWageArray = new Array();
-    while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random()*10)%3;
-        let empHrs = getWorkingHours(empCheck);
-        totalEmpHrs +=empHrs;
-        empDailyHrsAndWageArray.push({
-            dayNum : totalWorkingDays,
-            dailyHours:empHrs,
-            dailyWage:calcDailyWage(empHrs),
-            toString() {
-                return "\nDay" + this.dayNum + " => Working Hours is: "+this.dailyHours + " Wage earned = " +this.dailyWage
-            },
-        });
-    }
-    console.log("Printing objects: "+empDailyHrsAndWageArray);
+//UC11 A - UC11 D
+    let totalWages = empDailyHrsAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage >0)
+                                            .reduce((totalWage,dailyHrsAndWage)=> totalWage+= dailyHrsAndWage.dailyWage,0);
+    console.log("Total wage UC11A - "+totalWages);
+    let totalHours = empDailyHrsAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours >0)
+                                            .reduce((totalHours,dailyHrsAndWage)=>totalHours+= dailyHrsAndWage.dailyHours,0);
+    console.log("Total hours UC11A - "+totalHours); 
+    console.log("11B- Logging Full Work Days");
+    empDailyHrsAndWageArray.filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours == 8).
+                            forEach(dailyHrsAndWage=>process.stdout.write(dailyHrsAndWage.toString()));
+    
+    let partWorkingDaysString = empDailyHrsAndWageArray.filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours==4)
+                                                        .map(dailyHrsAndWage=>dailyHrsAndWage.toString());
+    console.log("\n11C- Part Working Days"+partWorkingDaysString);
+
+    let noWorkingDays = empDailyHrsAndWageArray.filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours==0)
+                                                .map(dailyHrsAndWage=>dailyHrsAndWage.dayNum);
+    console.log("11D- Non working days: "+noWorkingDays);
 }
