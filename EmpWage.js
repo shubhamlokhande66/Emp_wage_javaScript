a5753392493e81051153b1a75f55c63d6c25a83d
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 16:26:46 +0530

    Daily wage calculation

--- EmpWage.js
+++ EmpWage.js
@@ -1,14 +1,23 @@
-//UC1
+///UC2 --> Calculate Daily Employee Wage using switch
 
-console.log("Welcome to Employee Wage Javascript")
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-//UC1--> IF-ELSE, checking empkoyee is present or absent
-{
-    const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random()*10) % 2;
-    if (empCheck == IS_ABSENT){
-        console.log("UC-1 Employee is absent.");
-    } else {
-    console.log("UC-1 Employee is prsent");
-    }
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("UC2 - Emp Wage: " + empWage);
