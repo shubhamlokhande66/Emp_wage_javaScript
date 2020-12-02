f3307e2430938268b5b72bd69d9ee8bc59ef9881
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 16:11:32 +0530

    first commit

---
+++ EmpWage.js
@@ -0,0 +1,13 @@
+  
+console.log("Welcome to Employee Wage Javascript")
+
+//UC1--> IF-ELSE, checking empkoyee is present or absent
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random()*10) % 2;
+    if (empCheck == IS_ABSENT){
+        console.log("UC-1 Employee is absent.");
+    } else {
+    console.log("UC-1 Employee is prsent");
+    }
+}
