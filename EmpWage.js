6052ba3389c5fd2c2679a610cd7c57cd888f7173
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-02 18:10:41 +0530

    Worked and Earned Single Object

--- EmpWage.js
+++ EmpWage.js
@@ -1,23 +1,21 @@
-//UC9 Arrow functions
-const findTotal = (totalVal,dailyVal)=>{
-    return totalVal+dailyVal;
+//UC10 object creation
+{
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArray = new Array();
+    while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random()*10)%3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs +=empHrs;
+        empDailyHrsAndWageArray.push({
+            dayNum : totalWorkingDays,
+            dailyHours:empHrs,
+            dailyWage:calcDailyWage(empHrs),
+            toString() {
+                return "\nDay" + this.dayNum + " => Working Hours is: "+this.dailyHours + " Wage earned = " +this.dailyWage
+            },
+        });
+    }
+    console.log("Printing objects: "+empDailyHrsAndWageArray);
 }
-let count =0;
-let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
-let totalSalary = empDailyWageArray.filter(dailyWage=>dailyWage>0)
-                                    .reduce(findTotal,0);
-console.log("UC 9A : Wage with arrow => function: "+totalSalary);
-let nonWorkingDays = new Array();
-let partWorkingDays = new Array();
-let fullWorkingDays = new Array();
-empDailyHrsMap.forEach((value,key)=>{
-    if(value==8) 
-        fullWorkingDays.push(key);
-    else if(value==4)
-        partWorkingDays.push(key);
-    else 
-        nonWorkingDays.push(key);
-});
-console.log("Full working days: "+fullWorkingDays);
-console.log("Part working days: "+partWorkingDays);
-console.log("Non working days: "+nonWorkingDays);
